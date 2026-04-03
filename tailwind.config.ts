5a1a1c4fe069bbaec6c8c8fe356f5e3597f27261
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-04-03 08:18:43 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,10 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    // FIXED: Jalur ini akan mendeteksi folder app/components kamu di mana pun lokasinya
+    // Jalur ini wajib tanpa prefix /src/ agar Tailwind mendeteksi filemu
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}', 
   ],
   theme: {
     extend: {
